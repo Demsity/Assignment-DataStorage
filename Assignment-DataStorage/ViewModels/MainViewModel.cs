e782af74ee6dae03d80ff3154d824dc1e90390be
Demsity <simonbacklin@hotmail.com>
2023-02-28 10:41:27 +0100

    Added Basic App Layout and MVVM Func.

---
+++ Assignment-DataStorage/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_DataStorage.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableObject currentViewModel;
+
+    [RelayCommand]
+    private void GoToViewAll()
+    {
+        CurrentViewModel= new ViewAllViewModel();
+    }
+
+    [RelayCommand]
+    private void GoToViewSingle()
+    {
+        CurrentViewModel = new ViewSingleViewModel();
+    }
+
+    [RelayCommand]
+    private void GoToAddTicket()
+    {
+        CurrentViewModel = new AddTicketViewModel();
+    }
+
+    public MainViewModel()
+    {
+        currentViewModel = new ViewAllViewModel();
+    }
+}
---
+++ Assignment-DataStorage/ViewModels/ViewAllViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_DataStorage.ViewModels;
+
+public partial class ViewAllViewModel : ObservableObject
+{
+}
