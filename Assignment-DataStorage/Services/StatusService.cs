61888a55e690cd17ced7f584ab21e388f16d662c
Demsity <simonbacklin@hotmail.com>
2023-03-08 13:17:40 +0100

    Added Status Manager

---
+++ Assignment-DataStorage/Services/StatusService.cs
@@ -0,0 +1,54 @@
+﻿using Assignment_DataStorage.Contexts;
+using Assignment_DataStorage.Models.Entities;
+using Assignment_DataStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
+
+namespace Assignment_DataStorage.Services;
+
+internal class StatusService
+{
+
+    private static DataContext _dataContext = new();
+
+    public static async Task SaveStatusAsync(StatusModel model)
+    {
+        var _status = new StatusEntity
+        {
+            Status = model.Status,
+        };
+
+        _dataContext.Status.Add(_status);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    public static async Task<IEnumerable<StatusModel>> GetAllStatusAsync()
+    {
+        var _statuses = new ObservableCollection<StatusModel>();
+
+        foreach (var _status in await _dataContext.Status.ToListAsync())
+            _statuses.Add(new StatusModel
+            {
+                Id = _status.Id,
+                Status = _status.Status,
+            });
+
+        return _statuses;
+    }
+
+    public static async Task DeleteStatusAsync(StatusModel model)
+    {
+        var _status = await _dataContext.Status.FirstOrDefaultAsync(x => x.Status == model.Status);
+        if (_status != null)
+        {
+            _dataContext.Status.Remove(_status);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
--- Assignment-DataStorage/ViewModels/ManageStatusViewModel.cs
+++ Assignment-DataStorage/ViewModels/ManageStatusViewModel.cs
@@ -1,13 +1,55 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Assignment_DataStorage.Models;
+using Assignment_DataStorage.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Assignment_DataStorage.ViewModels;
 
-internal class ManageStatusViewModel : ObservableObject
+internal partial class ManageStatusViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private ObservableCollection<StatusModel> statuses = new();
 
+    [ObservableProperty]
+    private StatusModel selectedStatus = null!;
+
+    [ObservableProperty]
+    private StatusModel status = null!;
+
+    [RelayCommand]
+    private async void SaveStatusToDBAsync()
+    {
+        if (Status != null)
+        {
+            await StatusService.SaveStatusAsync(Status);
+            Status = new StatusModel();
+            populateStatusCollectionAsync();
+        }
+    }
+
+    [RelayCommand]
+    private async void DeleteStatusFromDBAsync()
+    {
+        if (SelectedStatus != null)
+        {
+            await StatusService.DeleteStatusAsync(SelectedStatus);
+            populateStatusCollectionAsync();
+        }
+    }
+    public ManageStatusViewModel()
+    {
+        populateStatusCollectionAsync();
+        Status = new StatusModel();
+    }
+
+    private async void populateStatusCollectionAsync()
+    {
+        Statuses = (ObservableCollection<StatusModel>)await StatusService.GetAllStatusAsync();
+    }
 }
